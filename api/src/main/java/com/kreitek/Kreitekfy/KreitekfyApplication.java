c95dfa64cc214b1ede38f32df4aa80df98c7064b
Eidursson <borealcode.edv@gmail.com>
2022-05-25 11:31:00 +0200

    commit inicial: estructura basica creada

---
+++ api/src/main/java/com/kreitek/Kreitekfy/KreitekfyApplication.java
@@ -0,0 +1,13 @@
+package com.kreitek.Kreitekfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KreitekfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KreitekfyApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/kreitek/Kreitekfy/KreitekfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kreitek.Kreitekfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KreitekfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
